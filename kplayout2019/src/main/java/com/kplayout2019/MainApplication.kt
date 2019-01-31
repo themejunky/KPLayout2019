c6f09ff2c5a743b2494205de1043c9407f0d6168
Alin Lastun <alinlastun@gmail.com>
2019-01-31 10:39:50 +0200

    string gae

--- kplayout2019/src/main/java/com/kplayout2019/MainApplication.kt
+++ kplayout2019/src/main/java/com/kplayout2019/MainApplication.kt
@@ -9,20 +9,9 @@ var redirectGP = ""
 class MainApplication: Application() {
 
     lateinit var MGAE :Module_GoogleAnalyticsEvents
-    var isFirstEntry:Boolean=false
     override fun onCreate() {
         super.onCreate()
         packageNameApp= packageName
-        MGAE= Module_GoogleAnalyticsEvents.getInstance(this,"UA-58480755-1")
+        MGAE= Module_GoogleAnalyticsEvents.getInstance(this,getString(R.string.id_google_analytics))
     }
-
-    /*   fun setEvents(mGAEA: Module_GoogleAnalyticsEvents?, events: String) {
-           val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-           isFirstEntry = sharedPreferences.getBoolean("first", false)
-           if (isFirstEntry && mGAEA != null) {
-               mGAEA.getEvents(R.string.layout_event, "Buttons", events)
-           } else {
-               Log.d("taferfare", isFirstEntry.toString() + " mgae este null")
-           }
-       }*/
 }
