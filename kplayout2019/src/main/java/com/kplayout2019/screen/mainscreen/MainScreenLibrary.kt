6190fb3434dcac5937d68a6554db3b04ea2e0ffa
alinlastun <alinlastun@gmail.com>
2019-02-07 12:12:14 +0200

    fixed bug

--- kplayout2019/src/main/java/com/kplayout2019/screen/mainscreen/MainScreenLibrary.kt
+++ kplayout2019/src/main/java/com/kplayout2019/screen/mainscreen/MainScreenLibrary.kt
@@ -8,7 +8,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.kplayout2019.MainApplication
 import com.kplayout2019.R
-import com.kplayout2019.databinding.MainScreenBinding
+import com.kplayout2019.databinding.MainScreenLibraryBinding
 import com.kplayout2019.screen.moreapps.MoreApps
 import com.kplayout2019.screen.wallpapers.Wallpapers
 import com.kplayout2019.utils.ApplyingTheme
@@ -20,13 +20,13 @@ import effects.ripper.water.themejunky.com.rippereffects.ManagerWaterEffects
 class MainScreenLibrary : AppCompatActivity() {
     private val mTools = Tools()
     private lateinit var mViewModel: MainScreenViewModel
-    private lateinit var mBinding: MainScreenBinding
+    private lateinit var mBinding: MainScreenLibraryBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView(this, R.layout.main_screenLibrary)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.main_screen_library)
         mViewModel = ViewModelProviders.of(this, ViewModelFactory()).get(MainScreenViewModel::class.java)
         mBinding.activity = this
         init()
